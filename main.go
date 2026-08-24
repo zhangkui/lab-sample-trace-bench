b9a93cde26cacc1fa6ff2b5172ae53ce666671af
zhangkui <zhangkui@users.noreply.github.com>
2026-08-24 14:25:33 +0800

    init: orphan green baseline BUG-005

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/zhangkui/lab-sample-trace-bench/internal/api"
+	"github.com/zhangkui/lab-sample-trace-bench/internal/service"
+	"github.com/zhangkui/lab-sample-trace-bench/internal/store"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	path := os.Getenv("LAB_SAMPLE_TRACE_BENCH_DB")
+	if path == "" {
+		path = "data/lab-sample-trace-bench.db"
+	}
+	repo, err := store.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer repo.Close()
+	app, err := service.New(repo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	addr := os.Getenv("LAB_SAMPLE_TRACE_BENCH_ADDR")
+	if addr == "" {
+		addr = ":8080"
+	}
+	log.Printf("lab-sample-trace-bench listening on %s", addr)
+	log.Fatal(http.ListenAndServe(addr, api.New(app).Handler()))
+}
